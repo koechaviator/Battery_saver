44d1de438ba7c07e6b0b35eecfe74abe2e0e92ea
koechaviator <144885672+koechaviator@users.noreply.github.com>
2023-10-28 06:25:32 +0300

    add react navigation and react device info

--- App.tsx
+++ App.tsx
@@ -1,94 +1,16 @@
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Alert,
-  Text,
-  Button,
-  TouchableOpacity,
-  View,
-  Image
-} from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './components/HomeScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
 
-function App(): JSX.Element {
+const Stack = createNativeStackNavigator();
+export default function App() {
   return (
-    <>
-    {/* <StatusBar barStyle = "dark-content" hidden = {false} backgroundColor = "#00BCD4" translucent = {true}/> */}
-    <SafeAreaView >
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        >
-        <View style={styles.sectionFirst}>
-          <View style={styles.header}>
-            <Text style={styles.title}>Battery Saver</Text>
-          </View>
-          <View style={styles.sectionDisplayBattery}>
-            <Image 
-              source={require('./images/low_battery.png')}
-              style={{width: 300, height: 250}}
-            />
-            {/* <Icon name="battery-0" size={30} color="#900" /> */}
-            <Text style={styles.percentage}>3%</Text>
-            <Text  style={styles.percentageAbout}>About 22m left</Text>
-          </View>
-        </View>
-        <View>
-          <Button onPress={()=>Alert.alert('Hello')} title="This looks great!" />
-          <TouchableOpacity onPress={()=>Alert.alert('Hello')} style={styles.button}>
-            <Text>Click me</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-    </>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionFirst: {
-    backgroundColor:"#ffff"
-  },
-  header:{
-    paddingVertical: 10,
-    paddingHorizontal:10,
-    backgroundColor:"#f5fbfc"
-  },
-  title: {
-    fontSize: 24,
-    color:'black',
-    fontWeight: '600',
-  },
-  sectionDisplayBattery:{
-    flex:1,
-    alignItems:'center',
-    marginHorizontal:15,
-    marginVertical:20,
-    paddingVertical: 10,
-    paddingHorizontal:10,
-  },
-  percentage: {
-    color:'#2ebfd9',
-    fontWeight: '600',
-    fontSize:50,
-  },
-  percentageAbout:{
-    fontSize:15,
-  },
-  button:{
-    flex:1,
-    alignItems:'center',
-    justifyContent:'center',
-    height:40,
-    backgroundColor:'#aedfe8'
-  }
-});
-
-export default App;
---
+++ components/HomeScreen.tsx
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import type {PropsWithChildren} from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { getUniqueId, getManufacturer } from 'react-native-device-info';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Alert,
+  Text,
+  Button,
+  TouchableOpacity,
+  View,
+  Image
+} from 'react-native';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function HomeScreen(): JSX.Element {
+  const [id,setId]=useState('')
+  getManufacturer().then((id)=>{
+    setId(id)
+  })
+  return (
+    <>
+    {/* <StatusBar barStyle = "dark-content" hidden = {false} backgroundColor = "#00BCD4" translucent = {true}/> */}
+    <SafeAreaView >
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        >
+        <View style={styles.sectionFirst}>
+          <View style={styles.header}>
+            <Text style={styles.title}>Battery Saver</Text>
+          </View>
+          <View style={styles.sectionDisplayBattery}>
+            <Image 
+              source={require('../images/low_battery.png')}
+              style={{width: 300, height: 250}}
+            />
+            {/* <Icon name="battery-0" size={30} color="#900" /> */}
+            <Text style={styles.percentage}>3%</Text>
+            <Text  style={styles.percentageAbout}>About 22m left {id}</Text>
+          </View>
+        </View>
+        <View>
+          <Button onPress={()=>Alert.alert('Hello')} title="This looks great!" />
+          <TouchableOpacity onPress={()=>Alert.alert('Hello')} style={styles.button}>
+            <Text>Click me</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionFirst: {
+    backgroundColor:"#ffff"
+  },
+  header:{
+    paddingVertical: 10,
+    paddingHorizontal:10,
+    backgroundColor:"#f5fbfc"
+  },
+  title: {
+    fontSize: 24,
+    color:'black',
+    fontWeight: '600',
+  },
+  sectionDisplayBattery:{
+    flex:1,
+    alignItems:'center',
+    marginHorizontal:15,
+    marginVertical:20,
+    paddingVertical: 10,
+    paddingHorizontal:10,
+  },
+  percentage: {
+    color:'#2ebfd9',
+    fontWeight: '600',
+    fontSize:50,
+  },
+  percentageAbout:{
+    fontSize:15,
+  },
+  button:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
+    height:40,
+    backgroundColor:'#aedfe8'
+  }
+});
+
+export default HomeScreen;
